4d0b2173303502a750c3c9ea0b9b2c3455605108
zongwenjin <zwj96812@163.com>
2021-12-15 01:08:54 +0800

    初始结构提交

---
+++ mymall-product/src/test/java/com/example/vinkin/product/MymallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinkin.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
